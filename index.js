6a6124deedf4c689610d5b97c646fd41c743056d
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-04 16:09:54 +0300

    Add cart markup

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
+const cartRoutes = require('./routes/cart')
 const coursesRoutes = require('./routes/courses')
 
 const app = express()
@@ -25,6 +26,7 @@ app.use(express.urlencoded({extended: true}))
 app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
+app.use('/cart', cartRoutes)
 
 const PORT = process.env.PORT || 3000
 

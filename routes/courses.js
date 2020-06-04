037c83352d589aa66d13d0f20bcc2c62643952f6
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-04 12:44:40 +0300

    Add dynamic params to routes

--- routes/courses.js
+++ routes/courses.js
@@ -1,14 +1,23 @@
 const {Router} = require('express')
-const Courses = require('../models/course')
+const Course = require('../models/course')
 const router = Router()
 
 router.get('/', async (req, res) => {
-  const courses = await Courses.getAll()
+  const courses = await Course.getAll()
 
   res.render('courses', {
     title: 'Courses',
     isCourses: true,
     courses,
+  })
+})
+
+router.get('/:id', async (req, res) => {
+  const course = await Course.getById(req.params.id)
+  res.render('course', {
+    layout: 'empty',
+    title: `Course ${course.title}`,
+    course,
   })
 })
 

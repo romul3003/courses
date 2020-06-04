bc9e57dbf4529ca1d4ebd32920960dc46b4ffbd2
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-04 11:47:12 +0300

    Add output of courses

--- routes/courses.js
+++ routes/courses.js
@@ -1,10 +1,14 @@
 const {Router} = require('express')
+const Courses = require('../models/course')
 const router = Router()
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  const courses = await Courses.getAll()
+
   res.render('courses', {
     title: 'Courses',
     isCourses: true,
+    courses,
   })
 })
 

4151918e61429d0a697fb8a36b595fa691140d52
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-04 14:34:51 +0300

    Add course edit

--- routes/courses.js
+++ routes/courses.js
@@ -1,24 +1,45 @@
-const {Router} = require('express')
-const Course = require('../models/course')
-const router = Router()
+const { Router } = require("express");
+const Course = require("../models/course");
+const router = Router();
 
-router.get('/', async (req, res) => {
-  const courses = await Course.getAll()
+router.get("/", async (req, res) => {
+  const courses = await Course.getAll();
 
-  res.render('courses', {
-    title: 'Courses',
+  res.render("courses", {
+    title: "Courses",
     isCourses: true,
     courses,
-  })
+  });
+});
+
+
+// edit route
+router.get("/:id/edit", async (req, res) => {
+  if (!req.query.allow) {
+    return res.redirect("/");
+  }
+
+  const course = await Course.getById(req.params.id);
+
+  res.render("course-edit", {
+    title: `Edit ${course.title}`,
+    course,
+  });
+});
+
+router.post('/edit', async (req, res) => {
+  await Course.update(req.body)
+  res.redirect('/courses')
 })
 
-router.get('/:id', async (req, res) => {
-  const course = await Course.getById(req.params.id)
-  res.render('course', {
-    layout: 'empty',
+// dynamic routes to selected course
+router.get("/:id", async (req, res) => {
+  const course = await Course.getById(req.params.id);
+  res.render("course", {
+    layout: "empty",
     title: `Course ${course.title}`,
     course,
-  })
-})
+  });
+});
 
-module.exports = router
+module.exports = router;

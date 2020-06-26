9b1d56c86733503da0428db8f15306a1252b65e4
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-26 21:52:54 +0300

    Add course removing from the cart

--- models/user.js
+++ models/user.js
@@ -30,9 +30,7 @@ const userSchema = new Schema({
 
 userSchema.methods.addToCart = function(course) {
   const items = [...this.cart.items]
-  const idx = items.findIndex(c => {
-    return c.courseId.toString() === course._id.toString()
-  })
+  const idx = items.findIndex(c => c.courseId.toString() === course._id.toString())
 
   if (idx >= 0) {
     items[idx].count = items[idx].count + 1
@@ -44,6 +42,20 @@ userSchema.methods.addToCart = function(course) {
   }
 
   this.cart = { items }
+  return this.save()
+}
+
+userSchema.methods.removeFromCart = function(id) {
+  let items = [...this.cart.items]
+  const idx = items.findIndex(c => c.courseId.toString() === id.toString())
+
+  if (items[idx].count === 1) {
+    items = items.filter(c => c.courseId.toString() !== id.toString())
+  } else {
+    items[idx].count--
+  }
+
+  this.cart = {items}
   return this.save()
 }
 
--- routes/cart.js
+++ routes/cart.js
@@ -5,6 +5,7 @@ const router = Router()
 function mapCartItems(cart) {
   return cart.items.map(c => ({
     ...c.courseId._doc,
+    id: c.courseId.id,
     count: c.count,
   }))
 }
@@ -37,7 +38,16 @@ router.post('/add', async (req, res) => {
 })
 
 router.delete('/remove/:id', async (req, res) => {
-  const cart = await Cart.remove(req.params.id)
+  await req.user.removeFromCart(req.params.id)
+  const user = await req.user
+    .populate('cart.items.courseId')
+    .execPopulate()
+
+  const courses = mapCartItems(user.cart)
+  const cart = {
+    courses, price: computePrice(courses)
+  }
+
   res.status(200).json(cart)
 })
 

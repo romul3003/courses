3ec51b3497d401955dc7d54d36155c7512cf68c5
Roman Zakrevskyi <romul3003@gmail.com>
2020-06-26 22:33:36 +0300

    Add Order model

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const homeRoutes = require('./routes/home')
 const addRoutes = require('./routes/add')
 const cartRoutes = require('./routes/cart')
 const coursesRoutes = require('./routes/courses')
+const ordersRoutes = require('./routes/orders')
 const User = require('./models/user')
 
 const app = express()
@@ -45,6 +46,7 @@ app.use('/', homeRoutes)
 app.use('/add', addRoutes)
 app.use('/courses', coursesRoutes)
 app.use('/cart', cartRoutes)
+app.use('/orders', ordersRoutes)
 
 const PORT = process.env.PORT || 3000
 
